4b3feb8166e38b707cb907ed9041cee3bd99939b
SaidRH <36697955+SaidRH@users.noreply.github.com>
2020-12-17 12:08:37 +0100

    Update SoundEffectsManager.cs

--- Assets/Scripts/SoundEffectsManager.cs
+++ Assets/Scripts/SoundEffectsManager.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
 public class SoundEffectsManager : MonoBehaviour {
 	[SerializeField] private AudioSource sFX1;
