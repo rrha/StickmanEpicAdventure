b9c2d7bda1f6404b2678f42a01bc54bd6b89ad4f
SaidRH <36697955+SaidRH@users.noreply.github.com>
2020-12-17 13:20:01 +0100

    Update BossSawAudio.cs

--- Assets/Scripts/BossSawAudio.cs
+++ Assets/Scripts/BossSawAudio.cs
@@ -1,20 +1,22 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class BossSawAudio : MonoBehaviour {
-	AudioSource sawSfx;
-	// Use this for initialization
-	void Start () {
-		sawSfx = this.GetComponent <AudioSource> ();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		if (transform.position.y >= -24.06045f){
-			sawSfx.volume = 0.5f;
-		}else if (transform.position.y <= -24.06045f){
-			sawSfx.volume = 0f;
-		}
-	}
+public class BossSawAudio : MonoBehaviour
+{
+    AudioSource sawSfx;
+    void Start()
+    {
+        sawSfx = this.GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        if (transform.position.y >= -24.06045f)
+        {
+            sawSfx.volume = 0.5f;
+        }
+        else if (transform.position.y <= -24.06045f)
+        {
+            sawSfx.volume = 0f;
+        }
+    }
 }
