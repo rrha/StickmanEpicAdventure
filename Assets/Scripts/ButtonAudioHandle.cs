dccc92eb214092d77441f339d96b3623e524b815
SaidRH <36697955+SaidRH@users.noreply.github.com>
2020-12-17 13:20:03 +0100

    Update ButtonAudioHandle.cs

--- Assets/Scripts/ButtonAudioHandle.cs
+++ Assets/Scripts/ButtonAudioHandle.cs
@@ -1,20 +1,22 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class ButtonAudioHandle : MonoBehaviour {
-	AudioSource buttonAudio;
-	float nextDamage = 0f;
-	float NextDamageRate = 4f;
-	// Use this for initialization
-	void Start () {
-		buttonAudio = this.GetComponent <AudioSource> ();
-	}
-	
-	void OnCollisionEnter2D (Collision2D collisionData) {
-		if (collisionData.gameObject.CompareTag("Player") && Time.time > nextDamage){
-			buttonAudio.Play ();
-			nextDamage = Time.time + NextDamageRate;
-		}
-	}
+public class ButtonAudioHandle : MonoBehaviour
+{
+    AudioSource buttonAudio;
+    float nextDamage = 0f;
+    float NextDamageRate = 4f;
+
+    void Start()
+    {
+        buttonAudio = this.GetComponent<AudioSource>();
+    }
+
+    void OnCollisionEnter2D(Collision2D collisionData)
+    {
+        if (collisionData.gameObject.CompareTag("Player") && Time.time > nextDamage)
+        {
+            buttonAudio.Play();
+            nextDamage = Time.time + NextDamageRate;
+        }
+    }
 }
