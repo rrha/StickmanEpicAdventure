34d22405d335246bec60bd4d14db33f0965f953d
SaidRH <36697955+SaidRH@users.noreply.github.com>
2020-12-17 13:20:06 +0100

    Update CactusHandle.cs

--- Assets/Scripts/CactusHandle.cs
+++ Assets/Scripts/CactusHandle.cs
@@ -1,27 +1,22 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class CactusHandle : MonoBehaviour {
-	public Transform endPoint;
-	public float timeToInvoke;
-	public float speed;
+public class CactusHandle : MonoBehaviour
+{
+    public Transform endPoint;
+    public float timeToInvoke;
+    public float speed;
+    void Update()
+    {
+        Invoke("MoveToEndPoint", timeToInvoke);
+        if (transform.position.x == endPoint.position.x)
+        {
+            Destroy(gameObject);
+        }
+    }
+    void MoveToEndPoint()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, endPoint.position, speed);
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		Invoke ("MoveToEndPoint",timeToInvoke);
-		if (transform.position.x == endPoint.position.x){
-			Destroy (gameObject);
-		}
-	}
-	void MoveToEndPoint (){
-		transform.position = Vector2.MoveTowards(transform.position, endPoint.position, speed);
-
-	}
+    }
 
 }
