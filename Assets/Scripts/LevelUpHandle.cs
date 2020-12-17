13afec0dbd69fbf7bbf98e91a0d7db1c26ca3de0
SaidRH <36697955+SaidRH@users.noreply.github.com>
2020-12-17 12:08:32 +0100

    Update LevelUpHandle.cs

--- Assets/Scripts/LevelUpHandle.cs
+++ Assets/Scripts/LevelUpHandle.cs
@@ -1,16 +1,9 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class LevelUpHandle : MonoBehaviour {
 	public GameObject[] snakes;
 	public GameObject nextLevelKey;
-	// Use this for initialization
-	void Start () {
-		
-	}
 
-	// Update is called once per frame
 	void Update () {
 		if (!snakes[0].activeInHierarchy&&!snakes[1].activeInHierarchy&& nextLevelKey!=null)
         {
